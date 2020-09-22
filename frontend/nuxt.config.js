7db6b0cd4f3762d559f8d830260320604308d1dc
Aydn Vasilyev <nevrikofficial@gmail.com>
2020-09-22 20:14:45 +0900

    signin page

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,9 +1,6 @@
-import colors from 'vuetify/es5/util/colors';
-
 export default {
   head: {
-    titleTemplate: 'Yet Another Todo',
-    title: 'Yet Another Todo',
+    titleTemplate: 'Yet Another Todo %s',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -26,18 +23,11 @@ export default {
     },
   },
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ['~/assets/colors.scss'],
     theme: {
-      dark: false,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
+        light: {
+          primary: '#3f51b5',
         },
       },
     },
---
+++ frontend/src/pages/signin.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-main class="signin-page">
+    <v-container class="fill-height" fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="12" sm="8" md="4" lg="3">
+          <v-card class="elevation-12">
+            <v-card-text>
+              <v-form>
+                <v-text-field
+                  autofocus
+                  placeholder="example@example.com"
+                  name="email"
+                  append-icon="mdi-email"
+                  type="text"
+                />
+
+                <v-text-field
+                  id="password"
+                  placeholder="123123123"
+                  name="password"
+                  append-icon="mdi-lock"
+                  type="password"
+                />
+              </v-form>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer />
+              <v-btn color="primary"> Sign in </v-btn>
+              <v-spacer />
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-main>
+</template>
+
+<script>
+export default {
+  head: {
+    title: '- Sign in',
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.signin-page {
+  background-color: $primary;
+}
+</style>
