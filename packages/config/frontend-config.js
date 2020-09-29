e6282519c39c2ddd082a23e48b477cbf44014dbe
Aydn Vasilyev <nevrikofficial@gmail.com>
2020-09-30 01:54:46 +0900

    fail: cancelling every change

--- packages/config/frontend-config.js
+++ packages/config/frontend-config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  graphqlAPI: process.env.GRAPHQL_API_URL || 'http://172.17.112.250:1234/admin/api',
+  graphqlAPI: process.env.GRAPHQL_API_URL || 'http://192.168.0.17:1234/admin/api',
   nuxt: {
-    port: process.env.PORT || 4321,
+    port: process.env.CLIENT_PORT || 4321,
     host: process.env.CLIENT_HOST || '0.0.0.0',
   },
 };
