653124f150e0494d65abe84c2a893546ff388728
Aydn Vasilyev <nevrikofficial@gmail.com>
2020-09-30 23:31:56 +0900

    get rid of git add in lint staged

--- packages/config/frontend-config.js
+++ packages/config/frontend-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  graphqlAPI: process.env.GRAPHQL_API_URL || 'http://192.168.0.17:1234/admin/api',
+  graphqlAPI: process.env.GRAPHQL_API_URL || 'http://localhost:1234/admin/api',
   nuxt: {
     port: process.env.CLIENT_PORT || 4321,
     host: process.env.CLIENT_HOST || '0.0.0.0',
