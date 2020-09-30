dbadda9f52a44fd78ba5e5f2904e8610046714e0
Aydn Vasilyev <nevrikofficial@gmail.com>
2020-09-30 23:40:19 +0900

    changed ports and hosts

--- packages/config/frontend-config.js
+++ packages/config/frontend-config.js
@@ -2,6 +2,6 @@ module.exports = {
   graphqlAPI: process.env.GRAPHQL_API_URL || 'http://localhost:1234/admin/api',
   nuxt: {
     port: process.env.CLIENT_PORT || 4321,
-    host: process.env.CLIENT_HOST || '0.0.0.0',
+    host: process.env.CLIENT_HOST || 'localhost',
   },
 };
